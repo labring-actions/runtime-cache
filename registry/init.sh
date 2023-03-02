94b6a2333b244c88470f99bde4af85e9ea028019
cuisongliu <cuisongliu@qq.com>
2023-03-02 15:46:19 +0800

    add fix registry

--- registry/init.sh
+++ registry/init.sh
@@ -3,8 +3,7 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 export readonly ARCH=${1:-amd64}
-export readonly NAME=${2:-$(basename "${PWD%/*}")}
-export readonly VERSION=${3:-2.8}
+export readonly VERSION=${2:-2.8}
 
 rm -rf cri
 rm -rf /tmp/runtime-cache/registry
