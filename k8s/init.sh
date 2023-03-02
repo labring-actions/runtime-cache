2fad0cf36af3076004c40dc997f735901df3689d
cuisongliu <cuisongliu@qq.com>
2023-03-02 17:13:36 +0800

    add k8s image

--- k8s/init.sh
+++ k8s/init.sh
@@ -6,22 +6,28 @@ export readonly ARCH=${1:-amd64}
 export readonly VERSION=${2:-1.25.0}
 
 rm -rf bin
-rm -rf /tmp/runtime-cache/kube
+rm -rf /tmp/runtime-cache/k8s
 mkdir -p "bin"
-mkdir -p "/tmp/runtime-cache/kube/$ARCH"
+mkdir -p "/tmp/runtime-cache/k8s/$ARCH"
 
 pushd "scripts" && {
   cp ../../scripts/common.sh .
 }
 popd
 
-sandboxImage="xxxx"
+sandboxImage=""
+
+pushd "/tmp/runtime-cache/k8s/$ARCH" && {
+  sudo sealos create --platform linux/$ARCH --short "ghcr.io/labring-actions/cache:kubernetes-v${VERSION}-$ARCH" &> mount
+  sandboxImage=$(cat $(cat mount)/images/shim/DefaultImageList | grep pause )
+}
+popd
 
 cat <<EOF >"Kubefile"
-FROM scratch
+FROM ghcr.io/labring-actions/cache:kubernetes-v${VERSION}-$ARCH
 MAINTAINER sealos
 LABEL init="init-cri.sh && bash init.sh" \
-      clean="clean.sh && bash clean-cri.sh \
+      clean="clean.sh && bash clean-cri.sh" \
       merge.sealos.io.type.kube="$VERSION" \
       sealos.io.type="rootfs" \
       sealos.io.version="v1beta1" \
