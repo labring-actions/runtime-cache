74f874b9347c8b249ec3e9dfa7fc2a225e93648f
cuisongliu <cuisongliu@qq.com>
2023-03-15 11:39:17 +0800

    add crio

--- build-image.sh
+++ build-image.sh
@@ -18,56 +18,56 @@ rm -rf cri-o/{cri,Kubefile,opt,scripts/common.sh,bin}
 pushd "registry" && {
   echo "build registry"
   bash init.sh ${ARCH} 2.8.1
-  sealos build -t dev-registry:2.8.1 .
+  sealos build -t dev-registry:2.8.1-${ARCH} .
 }
 popd
 
 pushd "sealos" && {
   echo "build sealos"
   bash init.sh ${ARCH} 4.1.6
-  sealos build --compress -t dev-sealos:4.1.6 .
+  sealos build --compress -t dev-sealos:4.1.6-${ARCH} .
 }
 popd
 
 pushd "tools" && {
   echo "build tools"
   bash init.sh ${ARCH}
-  sealos build -t dev-tools .
+  sealos build -t dev-tools:latest-${ARCH} .
 }
 popd
 
 pushd "docker" && {
   echo "build docker"
   bash init.sh ${ARCH} 20.10.23
-  sealos build -t dev-docker:20.10.23 .
+  sealos build -t dev-docker:20.10.23-${ARCH} .
 }
 popd
 
 pushd "cri-dockerd" && {
   echo "build cri-dockerd"
   bash init.sh ${ARCH} 0.2.6
-  sealos build -t dev-cri-dockerd:0.2.6 .
+  sealos build -t dev-cri-dockerd:0.2.6-${ARCH} .
 }
 popd
 
 pushd "k8s" && {
   echo "build k8s"
   bash init.sh ${ARCH} 1.23.17 1.23.0
-  sealos build --compress --debug -t dev-k8s:1.23.17 .
+  sealos build --compress --debug -t dev-k8s:1.23.17-${ARCH} .
 }
 popd
 
 pushd "containerd" && {
   echo "build containerd"
   bash init.sh ${ARCH} 1.6.19
-  sealos build --debug -t dev-containerd:1.6.19 .
+  sealos build --debug -t dev-containerd:1.6.19-${ARCH} .
 }
 popd
 
 pushd "runc" && {
   echo "build runc"
   bash init.sh ${ARCH} 1.1.4
-  sealos build --debug -t dev-runc:1.1.4 .
+  sealos build --debug -t dev-runc:1.1.4-${ARCH} .
 }
 popd
 
@@ -75,17 +75,17 @@ popd
 pushd "crun" && {
   echo "build crun"
   bash init.sh ${ARCH} 1.8.1
-  sealos build --debug -t dev-crun:1.8.1 .
+  sealos build --debug -t dev-crun:1.8.1-${ARCH} .
 }
 popd
 
 pushd "cri-o" && {
   echo "build cri-o"
   bash init.sh ${ARCH} 1.23.5
-  sealos build --debug -t dev-crio:1.23.5 .
+  sealos build --debug -t dev-crio:1.23.5-${ARCH} .
 }
 popd
 
-sealos merge -t dev-merge-docker-k8s:1.23.17 dev-k8s:1.23.17 dev-cri-dockerd:0.2.6 dev-docker:20.10.23 dev-tools dev-sealos:4.1.6 dev-registry:2.8.1 dev-tools
-sealos merge -t dev-merge-containerd-k8s:1.23.17 dev-k8s:1.23.17 dev-containerd:1.6.19 dev-runc:1.1.4 dev-tools dev-sealos:4.1.6 dev-registry:2.8.1 dev-tools
-sealos merge -t dev-merge-crio-k8s:1.23.17 dev-k8s:1.23.17 dev-crio:1.23.5 dev-crun:1.8.1 dev-tools dev-sealos:4.1.6 dev-registry:2.8.1 dev-tools
+sealos merge --platform linux/${ARCH} -t dev-merge-docker-k8s:1.23.17-${ARCH} dev-k8s:1.23.17-${ARCH} dev-cri-dockerd:0.2.6-${ARCH} dev-docker:20.10.23-${ARCH} dev-sealos:4.1.6-${ARCH} dev-registry:2.8.1-${ARCH} dev-tools:latest-${ARCH}
+sealos merge --platform linux/${ARCH} -t dev-merge-containerd-k8s:1.23.17-${ARCH} dev-k8s:1.23.17-${ARCH} dev-containerd:1.6.19-${ARCH} dev-runc:1.1.4-${ARCH}  dev-sealos:4.1.6-${ARCH} dev-registry:2.8.1-${ARCH} dev-tools:latest-${ARCH}
+sealos merge --platform linux/${ARCH} -t dev-merge-crio-k8s:1.23.17-${ARCH} dev-k8s:1.23.17-${ARCH} dev-crio:1.23.5-${ARCH} dev-crun:1.8.1-${ARCH}   dev-sealos:4.1.6-${ARCH} dev-registry:2.8.1-${ARCH} dev-tools:latest-${ARCH}
