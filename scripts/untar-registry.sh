407f7cc45571643b5f64363c41528865389d4c40
cuisongliu <cuisongliu@qq.com>
2023-04-20 10:52:36 +0800

    add kubelet en

--- scripts/untar-registry.sh
+++ scripts/untar-registry.sh
@@ -2,6 +2,6 @@
 
 source common.sh
 
-if [ -d ../registry/compressed/ ]; then find ../registry/compressed/ -type f -exec file {} \; | grep compressed | awk -F: '{print $1}' | while IFS='' read -r cpd; do tar -zxf "$cpd" -C ../registry && rm -rf "$cpd"; done; fi
+if [ -d ../registry/compressed/ ]; then find ../registry/compressed/ -type f -exec file {} \; | grep compressed | awk -F: '{print $1}' | while IFS='' read -r cpd; do tar -zxf "$cpd"  -C ../registry/docker && rm -rf "$cpd" ; done; fi
 
 logger "untar registry success"
